a274d6442eee219f1cac762b5d7c99e0c9f0a7be
Raphael Matteoni <raphaelmatteoni@gmail.com>
2024-07-03 15:00:33 -0300

    refactor to separate into more methods

--- weather_analysis.rb
+++ weather_analysis.rb
@@ -1,41 +1,40 @@
 # frozen_string_literal: true
 
-def find_day_with_smallest_temp_spread(file_path)
-  smallest_spread = Float::INFINITY
-  min_temp_smallest_spread = Float::INFINITY
-  max_temp_smallest_spread = Float::INFINITY
-  day_with_smallest_spread = nil
-
+def read_weather_data(file_path)
+  data = []
   File.foreach(file_path).with_index do |line, index|
     next if index <= 5
 
     columns = line.split
     next unless columns.size >= 3
 
-    day = columns[0].to_i
-    max_temp = columns[1].to_i
-    min_temp = columns[2].to_i
-    spread = max_temp - min_temp
-
-    if spread < smallest_spread
-      smallest_spread = spread
-      min_temp_smallest_spread = min_temp
-      max_temp_smallest_spread = max_temp
-      day_with_smallest_spread = day
-    end
+    data << {
+      day: columns[0].to_i,
+      max_temp: columns[1].to_i,
+      min_temp: columns[2].to_i
+    }
   end
+  data
+end
 
+def find_day_with_smallest_temp_spread(data)
+  data.min_by { |entry| entry[:max_temp] - entry[:min_temp] }
+end
+
+def find_data_from_file(file_path)
+  data = read_weather_data(file_path)
+  day_info = find_day_with_smallest_temp_spread(data)
   {
-    day: day_with_smallest_spread,
-    max_temp: max_temp_smallest_spread,
-    min_temp: min_temp_smallest_spread,
-    spread: smallest_spread
+    day: day_info[:day],
+    max_temp: day_info[:max_temp],
+    min_temp: day_info[:min_temp],
+    spread: day_info[:max_temp] - day_info[:min_temp]
   }
 end
 
 file_path = 'w_data.dat'
-data = find_day_with_smallest_temp_spread(file_path)
+data = find_data_from_file(file_path)
 
 puts "Day with the smallest temperature spread: #{data[:day]} " \
      "(max: #{data[:max_temp]}, min: #{data[:min_temp]}, " \
-     "spread: #{data[:spread]})"
+     "spread: #{data[:spread]}"
