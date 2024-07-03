3a19d843b8d10509404980c1ee28d167ed3112bb
Raphael Matteoni <raphaelmatteoni@gmail.com>
2024-07-03 14:54:56 -0300

    rubocoping

--- weather_analysis.rb
+++ weather_analysis.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
+
 def find_day_with_smallest_temp_spread(file_path)
   smallest_spread = Float::INFINITY
   min_temp_smallest_spread = Float::INFINITY
   max_temp_smallest_spread = Float::INFINITY
   day_with_smallest_spread = nil
-  
+
   File.foreach(file_path).with_index do |line, index|
     next if index <= 5
+
     columns = line.split
     next unless columns.size >= 3
-    
+
     day = columns[0].to_i
     max_temp = columns[1].to_i
     min_temp = columns[2].to_i
@@ -21,11 +24,18 @@ def find_day_with_smallest_temp_spread(file_path)
       day_with_smallest_spread = day
     end
   end
-  
-  {day: day_with_smallest_spread, max_temp: max_temp_smallest_spread, min_temp: min_temp_smallest_spread, spread: smallest_spread}
+
+  {
+    day: day_with_smallest_spread,
+    max_temp: max_temp_smallest_spread,
+    min_temp: min_temp_smallest_spread,
+    spread: smallest_spread
+  }
 end
 
 file_path = 'w_data.dat'
 data = find_day_with_smallest_temp_spread(file_path)
 
-puts "Day with the smallest temperature spread: #{data[:day]} (max: #{data[:max_temp]}, min: #{data[:min_temp]}, spread: #{data[:spread]})"
+puts "Day with the smallest temperature spread: #{data[:day]} " \
+     "(max: #{data[:max_temp]}, min: #{data[:min_temp]}, " \
+     "spread: #{data[:spread]})"
