d8ea34332c8beb3402595ff6298a557716c07347
Raphael Matteoni <raphaelmatteoni@gmail.com>
2024-07-03 19:52:50 -0300

    adjust soccer analysis to use data processor

--- soccer_analysis.rb
+++ soccer_analysis.rb
@@ -1,36 +1,24 @@
-# frozen_string_literal: true
-
-def read_soccer_data(file_path)
-  data = []
-  content = File.read(file_path)
-  pre_content = content.match(/<pre>(.*?)<\/pre>/m)[1]
-  pre_content.each_line.with_index do |line, index|
-    next if index < 2 || line.start_with?('   ---') || line.strip.empty?
-
-    columns = line.split(/\s+/)
-
-    data << {
-      team: columns[2],
-      goals_for: columns[7].to_i,
-      goals_against: columns[9].to_i,
-      difference: (columns[7].to_i - columns[9].to_i).abs
-    }
-  end
-  data
-end
+require_relative 'data_processor'
 
 def find_team_with_smallest_difference(data)
   data.min_by { |entry| entry[:difference] }
 end
 
 def find_data_from_file(file_path)
-  data = read_soccer_data("soccer.dat")
+  data = DataProcessor.process_data(file_path, skip_lines: 2) do |columns|
+    {
+      team: columns[1],
+      goals_for: columns[6].to_i,
+      goals_against: columns[8].to_i,
+      difference: (columns[6].to_i - columns[8].to_i).abs
+    }
+  end
   match_info = find_team_with_smallest_difference(data)
   {
     team: match_info[:team],
-    goals_for: match_info[:goals_for].to_i,
-    goals_against: match_info[:goals_against].to_i,
-    difference: (match_info[:goals_for].to_i - match_info[:goals_against].to_i).abs
+    goals_for: match_info[:goals_for],
+    goals_against: match_info[:goals_against],
+    difference: match_info[:difference]
   }
 end
 
